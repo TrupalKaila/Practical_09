014bd38959259891f5a75fd53a446470b0570346
Trupal Kaila <trupalkaila8@gmail.com>
2025-11-24 19:51:58 +0530

    Add project files.

---
+++ Practical-09/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical_09.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Test1()
+        {
+            ViewBag.str = "Hello World";
+            return View();
+        }
+        public ActionResult Test2()
+        {
+            return View();
+        }
+        public string Test3()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practical_09.Controllers;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController homeController = new HomeController();
+            var str = homeController.Test3();
+            Assert.AreEqual("Hello World", str);
+        }
+    }
+}
